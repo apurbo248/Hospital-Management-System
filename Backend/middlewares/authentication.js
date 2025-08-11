cb408673795b3cb4a01a506954356e0edbcf1bd2
apurbopal248 <apurbopal248@gmail.com>
2025-08-12 00:02:02 +1000

    Done user registration and login with validated email formate and token

--- Backend/middlewares/authentication.js
+++ Backend/middlewares/authentication.js
@@ -1,17 +1,48 @@
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+// Middleware to verify JWT token
+export const protect = async (req, res, next) => {
+  let token;
 
-  if (token == null)
-    return res.status(403).json({ msg: "You don't have any access token !" }); // If no token, return Unauthorized
+  try {
+    // Token comes from Authorization header -> "Bearer token"
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
 
-  jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
-    if (err) return res.sendStatus(403); // If token is invalid, return Forbidden
-    req.user = user;
-    next(); // Proceed to the next middleware or route handler
-  });
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Attach user to request object (excluding password)
+      req.user = await User.findById(decoded.id).select("-password");
+
+      if (!req.user) {
+        return res.status(401).json({ message: "User not found" });
+      }
+
+      next();
+    }
+
+    if (!token) {
+      return res.status(401).json({ message: "Not authorized, no token" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: "Not authorized, token failed" });
+  }
 };
 
-export default authenticateToken;
+// Middleware to check user roles (e.g., admin, doctor, receptionist, patient)
+export const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .json({ message: `Role (${req.user.role}) not authorized` });
+    }
+    next();
+  };
+};
--- Backend/routers/userRoutes.js
+++ Backend/routers/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { register } from "../controllers/usersController.js";
-
+import { userRegister,userLogin } from "../controllers/usersController.js";
 const router = express.Router();
 
 // User registration route
-router.post("/register", register);
+router.post("/user_registration", userRegister);
+//user login route
+router.post("/user_login", userLogin); 
 
 export default router;
